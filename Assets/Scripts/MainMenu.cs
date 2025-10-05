8804a36df2f89dc3efd90a8fec0bacafd7cdbc41
Reider <trunghieuy2006@gmail.com>
2025-10-05 23:53:24 +0700

    Add menu scenes with mainmenu script

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;               
+public class MainMenuManager : MonoBehaviour
+{
+    [Header("Buttons")]
+    public Button playButton;
+    public Button optionsButton;
+    public Button quitButton;
+
+    private void Start()
+    {
+        playButton.onClick.AddListener(OnPlayClicked);
+        optionsButton.onClick.AddListener(OnOptionsClicked);
+        quitButton.onClick.AddListener(OnQuitClicked);
+    }
+
+    public void OnPlayClicked()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void OnOptionsClicked()
+    {
+
+    }
+
+    public void OnQuitClicked()
+    {
+        Debug.Log("Quit Game!");
+        Application.Quit();
+    }
+}
