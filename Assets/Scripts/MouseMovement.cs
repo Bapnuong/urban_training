a39751e78b196ef8fbfe5fd6fd6205442f38f16c
Reider <trunghieuy2006@gmail.com>
2025-10-09 02:11:53 +0700

    Sửa lại góc nhìn camera

--- Assets/Scripts/MouseMovement.cs
+++ Assets/Scripts/MouseMovement.cs
@@ -2,10 +2,10 @@
 
 public class MouseMovement : MonoBehaviour
 {
+    public Transform playerBody; // tham chiếu tới thân người
     public float mouseSensitivity = 100f;
 
     float xRotation = 0f;
-    float yRotation = 0f;
 
     public float topClamp = -90f;
     public float bottomClamp = 90f;
@@ -13,7 +13,6 @@ public class MouseMovement : MonoBehaviour
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        
     }
 
     void Update()
@@ -21,11 +20,12 @@ public class MouseMovement : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        // Xoay camera (ngẩng lên / cúi xuống)
         xRotation -= mouseY;
-        yRotation += mouseX;
         xRotation = Mathf.Clamp(xRotation, topClamp, bottomClamp);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
 
-        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
-
+        // Xoay thân người (quay trái / phải)
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 }
