5e957c92b3b6fee8ca84eb9be9a6c283268012b5
Reider <trunghieuy2006@gmail.com>
2025-10-09 14:28:18 +0700

    Thêm minimap

---
+++ Assets/Scripts/MinimapFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class MinimapFollow : MonoBehaviour
+{
+    public Transform player;
+    public float height = 50f;
+
+    void LateUpdate()
+    {
+        if (player == null) return;
+
+        // Di chuyển theo vị trí người chơi
+        Vector3 newPos = player.position;
+        newPos.y += height;
+        transform.position = newPos;
+
+        // Xoay theo hướng người chơi
+        transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+    }
+}
